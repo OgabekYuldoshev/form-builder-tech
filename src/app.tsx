fc1620454688eae1ef970e918478bb77de200ff2
Ogabek Yuldoshev <yuldashoff@tutamail.com>
2026-02-02 12:29:16 +0500

    Refactor App component to use Mantine's AppShell for layout, including Header, Navbar, Main, and Aside sections. Wrap the application in MantineProvider for theming support.

--- src/app.tsx
+++ src/app.tsx
@@ -1,3 +1,27 @@
+import { AppShell, Group, Text } from '@mantine/core';
+
 export function App() {
-  return <div>Hello Developer!</div>;
+
+  return (
+    <AppShell
+      header={{ height: 60 }}
+      navbar={{ width: 300, breakpoint: 'md', collapsed: { mobile: true } }}
+      aside={{ width: 300, breakpoint: 'md', collapsed: { mobile: true } }}
+      padding="md"
+    >
+      <AppShell.Header>
+        <Group h="100%" px="md">
+          Header
+        </Group>
+      </AppShell.Header>
+      <AppShell.Navbar p="md">Navbar</AppShell.Navbar>
+      <AppShell.Main>
+        <Text>This is the main section, your app content here.</Text>
+        <Text>AppShell example with all elements: Navbar, Header, Aside, Footer.</Text>
+        <Text>All elements except AppShell.Main have fixed position.</Text>
+        <Text>Aside is hidden on on md breakpoint and cannot be opened when it is collapsed</Text>
+      </AppShell.Main>
+      <AppShell.Aside p="md">Aside</AppShell.Aside>
+    </AppShell>
+  );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,12 @@ import "./global.css";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { App } from "./app";
+import { MantineProvider } from "@mantine/core";
 
 createRoot(document.getElementById("root") as HTMLElement).render(
   <StrictMode>
+    <MantineProvider>
     <App />
+    </MantineProvider>
   </StrictMode>
 );

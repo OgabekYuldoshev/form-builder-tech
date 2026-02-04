8b899392a0142621a282cd091671439d8b565884
Ogabek Yuldoshev <yuldashoff@tutamail.com>
2026-02-04 14:22:47 +0500

    Update package dependencies to include '@atlaskit/pragmatic-drag-and-drop-hitbox' and enhance ElementWrapper component with drag-and-drop functionality. Add styling for dragging state in SCSS.

--- src/features/builder/components/element-wrapper/element-wrapper.tsx
+++ src/features/builder/components/element-wrapper/element-wrapper.tsx
@@ -3,6 +3,11 @@ import { Box } from "@mantine/core";
 import styles from "./element-wrapper.module.scss";
 import type { ElementInstance } from "../../types";
 import { useBuilderStore } from "../../hooks/use-builder-store";
+import { useEffect, useRef, useState } from "react";
+import { draggable, dropTargetForElements } from '@atlaskit/pragmatic-drag-and-drop/element/adapter';
+import {combine } from '@atlaskit/pragmatic-drag-and-drop/combine';
+import invariant from "invariant";
+
 
 interface ElementWrapperProps {
     children: React.ReactNode;
@@ -10,12 +15,40 @@ interface ElementWrapperProps {
 }
 
 export function ElementWrapper({ children, element }: ElementWrapperProps) {
+    const [isDragging, setIsDragging] = useState(false);
+    const elementRef = useRef<HTMLDivElement>(null);
     const handleSelect = useBuilderStore((state) => state.handleSelect);
     const selectedElementId = useBuilderStore((state) => state.selectedElementId);
 
+    useEffect(() => {
+        invariant(elementRef.current, "Element ref not found");
+
+        return combine(
+            draggable({
+                element: elementRef.current,
+                onDragStart(){
+                    setIsDragging(true);
+                },
+                onDrop(){
+                    setIsDragging(false);
+                }
+            }),
+            dropTargetForElements({
+                element: elementRef.current,
+                onDrop(event){
+                    console.log(event)
+                }
+            })
+        )
+    }, []);
+
+
+
     return (
         <Box
+            ref={elementRef}
             data-selected={selectedElementId === element.id}
+            data-dragging={isDragging}
             className={styles.wrapper}
             onClick={() => handleSelect(element.id)}
         >
